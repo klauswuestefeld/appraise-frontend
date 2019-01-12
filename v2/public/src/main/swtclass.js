277244cc77c27cf0a13cef7d446f196f76accda7
carlosdaniel99 <cdmnkg@gmail.com>
2019-01-12 20:53:38 -0200

    barra menu

---
+++ v2/public/src/main/swtclass.js
@@ -0,0 +1,49 @@
+/*function swtclass(){
+    class1 = document.getElementById('tst').className; 
+class1 = document.getElementById('tst').className;
+    if(class1 == 'tab'){
+       document.getElementById('tst').className = 'tab selected-tab';
+   }else{
+       document.getElementById('tst').className = 'tab';
+   }
+}*/
+
+function switch1(){
+	class1 = document.getElementById('appraisals-tab').className; 
+	if(class1 == 'tab'){
+       document.getElementById('appraisals-tab').className = 'tab selected';
+       document.getElementById('admin-tab').className = 'tab';
+   }
+};
+
+function switch2(){
+	class2 = document.getElementById('admin-tab').className; 
+	if(class2 == 'tab'){
+       document.getElementById('admin-tab').className = 'tab selected';
+       document.getElementById('appraisals-tab').className = 'tab';
+   }
+};
+
+/*document.getElementById.onclick = function() {swtclass()};*/
+/*alert("goool");
+
+	var el = document.getElementById('menu');
+	el.addEventListener('click', function(e) {
+	alert(e.target.id);
+	if (e == 'bar1' ){
+		document.getElementById('bar1').className = 'tab selected-tab';
+		document.getElementById('bar2').className = 'tab';
+	else{
+		document.getElementById('bar2').className = 'tab selected-tab';
+		document.getElementById('bar1').className = 'tab';}
+	});
+}); */	
+		/*class1 = document.getElementById(clickid).className;
+		if(class1 == 'tab'){
+       		document.getElementById('tst').className = 'tab selected-tab';}
+        else{
+      	    document.getElementById('tst').className = 'tab';}*/
+
+
+
+
