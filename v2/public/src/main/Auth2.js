ae3c7555818f776159a8797c4188e7bdf120ea8e
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2019-01-22 17:24:40 -0200

    Google Auth

--- v2/public/src/main/Auth2.js
+++ v2/public/src/main/Auth2.js
@@ -3,7 +3,7 @@ Percy.initAuth = function() {
 
     gapi.load('auth2', function() {
         Percy.auth2 = gapi.auth2.init({
-            client_id: '902472309288-h34e3l0vo9e3bkr4dc29f4bco99q0t9s.apps.googleusercontent.com'
+            client_id: '696992508397-9pna2ebcfkt3olr2hukd9q95v21t1iud.apps.googleusercontent.com'
         });
 
         // auth2.attachClickHandler(
@@ -13,27 +13,32 @@ Percy.initAuth = function() {
         //     onGoogleLoginError
         // );
 
-        Percy.auth2.then(function authThen() {
+        Percy.auth2.then(function onInit() {
+            console.log("INIT. SignedIn:", Percy.auth2.isSignedIn.get());
 
-            var signedIn = Percy.auth2.isSignedIn.get();
-
-            if (signedIn) {
+            Percy.auth2.isSignedIn.listen(function onSignInChanged(isSignedIn) {
+                console.log("IS SIGNED IN:", isSignedIn);
                 console.log(Percy.auth2.currentUser.get().getAuthResponse().id_token);
-                var logged = document.getElementById("logged");
-                logged.classList.add("show");
-            } else {
-                var login = document.getElementById("login");
-                login.classList.add("show");
-            }
-
+            });
         });
-
     });
 
 };
 
 Percy.loginClicked = function () {
-    Percy.auth2.signIn();
+    if (Percy.auth2.isSignedIn.get()) {
+        console.log("SIGNED IN");
+        Percy.auth2.signOut();
+        console.log("AFTER SIGNOUT:", Percy.auth2.isSignedIn.get());
+//        var logged = document.getElementById("logged");
+//        logged.classList.add("show");
+    } else {
+        console.log("NOT SIGNED IN.");
+        Percy.auth2.signIn();
+//        var login = document.getElementById("login");
+//        login.classList.add("show");
+    }
+
 };
 
 
