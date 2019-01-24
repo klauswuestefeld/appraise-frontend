47cd05a49aca6f111f3ff98e326a7fc9392f91be
carlosdaniel99 <cdmnkg@gmail.com>
2019-01-24 11:27:41 -0200

    Minor

--- v2/public/src/main/Transition.js
+++ v2/public/src/main/Transition.js
@@ -7,15 +7,24 @@ window.addEventListener("load", function() {
 function logScreen(){
 	document.getElementById("session").style.display = "none";
 	document.getElementById("menu").style.display = "none";
+	document.getElementById("conLog").style.display = "flex";
+	document.getElementById("conAdm").style.display = "none";
+	document.getElementById("conApp").style.display = "none";
 }
 
 function admScreen(){
 	document.getElementById("session").style.display = "flex";
 	document.getElementById("menu").style.display = "flex";
+	document.getElementById("conAdm").style.display = "flex";
+	document.getElementById("conLog").style.display = "none";
+	document.getElementById("conApp").style.display = "none";
 }
 
 
 function appScreen(){
 	document.getElementById("session").style.display = "flex";
 	document.getElementById("menu").style.display = "flex";
+	document.getElementById("conApp").style.display = "flex";
+	document.getElementById("conAdm").style.display = "none";
+	document.getElementById("conLog").style.display = "none";
 }
