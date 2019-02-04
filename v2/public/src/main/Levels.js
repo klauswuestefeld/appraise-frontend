cc7026e437eaef294326dbd09c8a7c454dedcb88
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2019-02-04 20:01:23 -0200

    Minor

--- v2/public/src/main/Levels.js
+++ v2/public/src/main/Levels.js
@@ -1,5 +1,7 @@
+/*
 window.addEventListener("load", function() {
 	document.getElementById("").onclick = function() {disableSave()};
 	for (iLoop = 1; iLoop <= totalCampos; iLoop++) {
         document.write("<div id='div"+iLoop+"' class='normal'></div>");}
 });
+*/
