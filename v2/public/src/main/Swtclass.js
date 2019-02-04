c6412b377d1bc9cbd2377508898ad4a8fbd6ddb7
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2019-02-04 20:02:26 -0200

    Selecao das abas de menu respeitando outras classes.

--- v2/public/src/main/Swtclass.js
+++ v2/public/src/main/Swtclass.js
@@ -1,18 +1,17 @@
 function change(el) {
 if (el == 'appraisals-tab'){
-   document.getElementById('appraisals-tab').className = 'tab selected';
-   document.getElementById('admin-tab').className = 'tab';
+   document.getElementById('appraisals-tab').classList.add('selected');
+   document.getElementById('admin-tab').classList.remove('selected');
 }
 else{
-   document.getElementById('appraisals-tab').className = 'tab';
-   document.getElementById('admin-tab').className = 'tab selected';
+   document.getElementById('appraisals-tab').classList.remove('selected');
+   document.getElementById('admin-tab').classList.add('selected');
 }
 }
 
-
-    window.addEventListener("load", function() {
-     document.getElementById("admin-tab").onclick = function() {change("admin-tab")};   
-     document.getElementById("appraisals-tab").onclick = function() {change("appraisals-tab")};
-    });
+window.addEventListener("load", function() {
+   document.getElementById("admin-tab").onclick = function() {change("admin-tab")};
+   document.getElementById("appraisals-tab").onclick = function() {change("appraisals-tab")};
+});
 
 
