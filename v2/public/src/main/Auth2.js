7f34cdc7766bed7c36d8a51bde46ec43e34f7fb5
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2019-03-11 19:48:02 -0300

    Invalid cookiePolicy exception caught

--- v2/public/src/main/Auth2.js
+++ v2/public/src/main/Auth2.js
@@ -1,10 +1,27 @@
 
+var i = 1;
+
+
 Percy.initAuth = function() {
 
     gapi.load('auth2', function() {
-        Percy.auth2 = gapi.auth2.init({
-            client_id: '696992508397-9pna2ebcfkt3olr2hukd9q95v21t1iud.apps.googleusercontent.com'
-        });
+        try {
+            Percy.auth2 = gapi.auth2.init({
+                client_id: '696992508397-9pna2ebcfkt3olr2hukd9q95v21t1iud.apps.googleusercontent.com'
+            });
+            Percy.auth2.then(function onInit() {
+                console.log("INIT. SignedIn:", Percy.auth2.isSignedIn.get());
+
+                Percy.auth2.isSignedIn.listen(function onSignInChanged(isSignedIn) {
+                    console.log("IS SIGNED IN:", isSignedIn);
+                    console.log(Percy.auth2.currentUser.get().getAuthResponse().id_token);
+                });
+            });
+        } catch (err) {
+            console.log(err.message);
+            if (err.message.includes("cookiePolicy"))
+                console.log("Serve using http server to avoid this error.");
+        }
 
         // auth2.attachClickHandler(
         //     element,
@@ -12,17 +29,7 @@ Percy.initAuth = function() {
         //     onGoogleLoginSuccess,
         //     onGoogleLoginError
         // );
-
-        Percy.auth2.then(function onInit() {
-            console.log("INIT. SignedIn:", Percy.auth2.isSignedIn.get());
-
-            Percy.auth2.isSignedIn.listen(function onSignInChanged(isSignedIn) {
-                console.log("IS SIGNED IN:", isSignedIn);
-                console.log(Percy.auth2.currentUser.get().getAuthResponse().id_token);
-            });
-        });
     });
-
 };
 
 Percy.loginClicked = function () {
