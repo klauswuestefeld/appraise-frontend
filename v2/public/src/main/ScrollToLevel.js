ea7967bc7f5e35d89aaafeea308d5d4129b2d7e8
carlosdaniel99 <cdmnkg@gmail.com>
2019-04-04 16:44:20 -0300

    Minor

--- v2/public/src/main/ScrollToLevel.js
+++ v2/public/src/main/ScrollToLevel.js
@@ -1,8 +1,10 @@
 window.addEventListener("load", function() {
-  ScrollToLevel();
+  scrollToLevel();
+  document.getElementById("situation-appraisals-empty").onclick = function() {scrollToLevel()};
+  document.getElementById("situation-appraisals-filled").onclick = function() {scrollToLevel()};
   });
 
-  function ScrollToLevel(){
+  function scrollToLevel(){
   	var levelScroll = document.getElementById("id-your-level");
     levelScroll.scrollIntoView({behavior: "smooth", block: "center", inline: "center"});
   }
