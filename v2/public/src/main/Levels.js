42d0487e94170c69d0627af2ea6e0df17d49ffae
carlosdaniel99 <cdmnkg@gmail.com>
2019-04-09 11:54:15 -0300

    Keep-cards e níveis hard-coded

--- v2/public/src/main/Levels.js
+++ v2/public/src/main/Levels.js
@@ -15,23 +15,23 @@ function otherLevels(){
     var myLevel = document.getElementById('id-level');
     var spanLevel = document.getElementById('span-level');
     var appraisal = document.getElementById("appraisals")
-    for (i = 10; i >= decreasing; i--) {
+    /*for (i = 10; i >= decreasing; i--) {
       var levelClone   = myLevel.cloneNode(false);
       levelClone.id = "id-extra+"+i;
       var beforeLevel = document.getElementById('id-level');
       appraisal.insertBefore(levelClone, beforeLevel);
       var levelSpan   = spanLevel.cloneNode(true);
       levelClone.appendChild(levelSpan);
-      levelSpan.innerHTML=i;
-	      if (i == 1){
-	      	var card = document.getElementById('id-card');
+      levelSpan.innerHTML=i;*/
+	      
+	      var card = document.getElementById('id-card');
 	    	var newCard   = card.cloneNode(true);
-	    	document.getElementById("id-extra+2").appendChild(newCard);
-	      }
-	      if (i == 2){
+	    	document.getElementById("keep+2").appendChild(newCard);
+	      
+	      /*if (i == 2){
 	      	document.getElementById("id-level").classList.add("drag-target");
 	      }
-    }
+    }*/
 
     for (i = 1; i <= 29; i++) {
       var card = document.getElementById('id-card');
@@ -39,14 +39,14 @@ function otherLevels(){
 	      if (i == 1){
 	      	document.getElementById("id-card").classList.add("dragging-card");
 	      }
-	      document.getElementById("id-level").appendChild(newCard);
+	      document.getElementById("keep-your").appendChild(newCard);
 	      if (i > 1){
 	 		document.getElementById("id-card").classList.remove("dragging-card");
 	      	newCard.id = "ghost-card"+i;
 	      }
     }
 
-    for (i = 1; i <= increasing; i++) {
+    /*for (i = 1; i <= increasing; i++) {
       document.getElementById("id-level").classList.remove("drag-target");
       var levelClone   = myLevel.cloneNode(false);
       levelClone.id = "id-extra-"+i;
@@ -54,18 +54,18 @@ function otherLevels(){
       var levelSpan = spanLevel.cloneNode(true);
       levelClone.appendChild(levelSpan);
       levelSpan.innerHTML=-i;
-    }
+    }*/
 
 	    for (i=1; i <= 6; i++){
 		    var card = document.getElementById('id-card');
 		    var newCard   = card.cloneNode(true);
-		    document.getElementById("id-extra-1").appendChild(newCard);
+		    document.getElementById("keep-1").appendChild(newCard);
 	    }
 
 	    for (i=1; i <= 3; i++){
 		    var card = document.getElementById('id-card');
 		    var newCard   = card.cloneNode(true);
-		    document.getElementById("id-extra-2").appendChild(newCard);
+		    document.getElementById("keep-2").appendChild(newCard);
 	    }
 
 }
