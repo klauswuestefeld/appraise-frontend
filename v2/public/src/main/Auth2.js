be0008ec0cd406dd72a01812cbe435c00044b8e6
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2019-04-17 09:39:31 -0300

    Google Auth calling backend

--- v2/public/src/main/Auth2.js
+++ v2/public/src/main/Auth2.js
@@ -1,20 +1,67 @@
 "use strict";
 
+function getUserProfile(token) {
+  console.log('GET USER PROFILE');
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+         console.log("USER PROFILE:", xhttp.responseText);
+      }
+  };
+  xhttp.open("GET", "http://api.appraise.live:8080/auth-google?google-id-token=" + token, true);
+  xhttp.send();
+}
+
+
+
+//    function backendAuth(token) {
+//        var req = new XMLHttpRequest();
+//        req.open("GET", 'auth-google?google-id-token=' + token);
+//        req.responseType = "json";
+//        req.timeout = 5000;
+//        req.onload = function () {
+//            if (req.status === 200) console.log(req.response);
+//            else req.onerror();
+//        };
+//        req.onerror = req.ontimeout = function () {
+//           console.log('Error', req.status, req.statusText);
+//        };
+//        req.send();
+//    }
+
+
+function getUserProfileOnSignIn(isSignedIn) {
+  console.log("IS SIGNED IN:", isSignedIn);
+  if (!isSignedIn)
+   return;
+
+  var token = Percy.auth2.currentUser.get().getAuthResponse().id_token;
+  console.log("TOKEN", token);
+  getUserProfile(token);
+}
+
+function onUserChanged(user) {
+  console.log(user);
+}
+
+function loginClicked()  { Percy.auth2.signIn(); }
+function logoutClicked() { Percy.auth2.signOut(); }
 
 Percy.initAuth = function() {
 
     gapi.load('auth2', function() {
         try {
             Percy.auth2 = gapi.auth2.init({
-                client_id: '696992508397-9pna2ebcfkt3olr2hukd9q95v21t1iud.apps.googleusercontent.com'
+                client_id: '902472309288-h34e3l0vo9e3bkr4dc29f4bco99q0t9s.apps.googleusercontent.com' //Appraise
+                //client_id: '696992508397-9pna2ebcfkt3olr2hukd9q95v21t1iud.apps.googleusercontent.com' //Percival
             });
-            Percy.auth2.then(function onInit() {
-                console.log("INIT. SignedIn:", Percy.auth2.isSignedIn.get());
 
-                Percy.auth2.isSignedIn.listen(function onSignInChanged(isSignedIn) {
-                    console.log("IS SIGNED IN:", isSignedIn);
-                    console.log(Percy.auth2.currentUser.get().getAuthResponse().id_token);
-                });
+            Percy.auth2.then(function onInit() {
+              document.getElementById('logout').onclick = logoutClicked;
+              document.getElementById('login-button').onclick = loginClicked;
+              Percy.auth2.isSignedIn.listen(getUserProfileOnSignIn);
+              Percy.auth2.currentUser.listen(onUserChanged);
+
             });
         } catch (err) {
             console.log(err.message);
@@ -22,41 +69,5 @@ Percy.initAuth = function() {
                 console.log("Serve using http server to avoid this error.");
         }
 
-        // auth2.attachClickHandler(
-        //     element,
-        //     {},
-        //     onGoogleLoginSuccess,
-        //     onGoogleLoginError
-        // );
     });
-};
-
-Percy.loginClicked = function () {
-    if (Percy.auth2.isSignedIn.get()) {
-        console.log("SIGNED IN");
-        Percy.auth2.signOut();
-        console.log("AFTER SIGNOUT:", Percy.auth2.isSignedIn.get());
-//        var logged = document.getElementById("logged");
-//        logged.classList.add("show");
-    } else {
-        console.log("NOT SIGNED IN.");
-        Percy.auth2.signIn();
-        console.log("AFTER SIGN IN.");
-
-//        var login = document.getElementById("login-button");
-//        login.classList.add("show");
-    }
-
-};
-
-
-
-Percy.authenticator = {
-
-  login: function() {
-      this.user = 1;
-  },
-
-  user: 0
-
 };
