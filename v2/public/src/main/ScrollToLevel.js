037bfa7c2f9a7e9ad7b4765d62d67beb0a43cc70
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2019-04-18 17:48:28 -0300

    Minor

--- v2/public/src/main/ScrollToLevel.js
+++ v2/public/src/main/ScrollToLevel.js
@@ -2,10 +2,9 @@ window.addEventListener("load", function() {
 	setTimeout (function () {
   	scrollToLevel();
   }, 100);
-  });
+});
 
-  function scrollToLevel(){
-  	var levelScroll = document.getElementById("id-your-level");
-    levelScroll.scrollIntoView({behavior: "smooth", block: "center", inline: "center"});
-  }
-
+function scrollToLevel(){
+	var levelScroll = document.getElementById("id-your-level");
+  levelScroll.scrollIntoView({behavior: "smooth", block: "center", inline: "center"});
+}
