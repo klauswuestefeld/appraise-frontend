9f0fd51dd0cfd4bb146b9575052a333f57af3d4f
carlosdaniel99 <cdmnkg@gmail.com>
2019-04-25 12:55:36 -0300

    Real people on cards - beginning

--- v2/public/src/main/Appraisals.js
+++ v2/public/src/main/Appraisals.js
@@ -1,7 +1,16 @@
 'use strict';
 
+function cloneTemplateAsCardOnLevel(cardName, level,picture) {
+  var card = document.getElementById('id-card');
+  var newCard = card.cloneNode(true);
+  newCard.classList.remove('force-display-none');
+  newCard.id = 'card-' + cardName;
+  document.getElementById(level).appendChild(newCard);
+}
+
 function showAppraisal(appraisal) {
 	console.log(appraisal);
+	cloneTemplateAsCardOnLevel(appraisal.name,'keep-your',appraisal.picture);
 }
 
 function showAppraisals(appraisals) {
