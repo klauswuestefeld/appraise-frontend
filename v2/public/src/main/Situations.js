158436871f9a2b6f165a05fce1b77f893f2de837
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2019-04-30 15:32:19 -0300

    WIP: Situations working for elements that have the situation class

--- v2/public/src/main/Situations.js
+++ v2/public/src/main/Situations.js
@@ -4,18 +4,36 @@ var Situations = {};
 Situations.all = [];
 Situations.index = -1;
 
+function clearBody() {
+  const elementsCopy = Array.from(document.body.childNodes);
+  elementsCopy.forEach(function (element) {
+    document.body.removeChild(element);
+  });
+}
+
+function displaySituation(situation) {
+  situation = situation;
+  console.log(situation);
+  clearBody();
+  Situations.templates.forEach(function (element) {
+    if (element.classList && element.classList.contains(situation))
+      document.body.appendChild(element);
+  });
+}
+
 function nextSituation(direction) {
   Situations.index = (Situations.index + direction) % Situations.all.length;
   if (Situations.index == -1)
     Situations.index = Situations.all.length -1;
-  window.location.hash = Situations.all[Situations.index];
+  var situation = Situations.all[Situations.index];
+  window.location.hash = situation;
+  displaySituation(situation);
 }
 
-function checkKey(e) {
+function onSituationKey(e) {
     e = e || window.event;
-    console.log('ALT:' + e.altKey,e.ctrlKey, e.keyCode);
     if (!e.ctrlKey) return;
-    if (!e.altKey) return;
+    if (!e.altKey)  return;
     if (e.keyCode == '188') { // ',' key
       if (Situations.index == -1)
         Situations.index = 0;
@@ -37,49 +55,29 @@ function findSituations(result, element) {
   });
 }
 
-function displaySituation(situation) {
-  document.body.childNodes.forEach(function (element) {
-    console.log('REMOVING', element, element.id);
-    document.body.removeChild(element);
-  });
-  document.getElementById('situation-template').childNodes.forEach(function (element) {
-    console.log('MOVING', element, element.id);
-    document.body.appendChild(element);
-  });
-
-}
-
 function displayBody() {
     document.getElementsByTagName('body')[0].classList.remove('force-display-none');
 }
 
-function initSituations(onLoadWithoutSituationHash) {
-  Situations.onLoadWithoutSituationHash = onLoadWithoutSituationHash;
-}
-
-window.addEventListener('load', function() {
-
+function startSituations() {
   findSituations(Situations.all, document.body);
 
-  document.onkeydown = checkKey;
-  if (window.location.hash.startsWith('#sit-'))
-    displaySituation(window.location.hash);
-
+  Situations.templates = Array.from(document.body.childNodes);
+  clearBody();
   displayBody();
 
-  if (window.location.hash != ('#situation-certainty'))
-    document.getElementById('certainty').classList.add('force-display-none');
-  registerSituation('login', loginScreen);
-  registerSituation('magic-login', magicLogScreen);
-  registerSituation('logout', dropdownScreen);
-  registerSituation('admin-empty', adminEmptyScreen);
-  registerSituation('admin-filled', adminFilledScreen);
-  registerSituation('admin-error', adminErrorScreen);
-  registerSituation('admin-ok', adminOkScreen);
-  registerSituation('appraise-empty', appraiseEmptyScreen);
-  registerSituation('appraise-filled', appraiseFilledScreen);
-  registerSituation('certainty', certaintyScreen);
-  document.getElementById('easter-egg').ondblclick = function() {easterEgg()};
+  document.onkeydown = onSituationKey;
 
-  Situations.onLoadWithoutSituationHash();
-});
+  if (window.location.hash.startsWith('#sit-'))
+    displaySituation(window.location.hash.substr(1));
+  else
+    Situations.onLoadWithoutSituationHash();
+
+}
+
+function initSituations(onLoadWithoutSituationHash) {
+  Situations.onLoadWithoutSituationHash = onLoadWithoutSituationHash;
+  window.addEventListener('load', startSituations);
+}
+
+
