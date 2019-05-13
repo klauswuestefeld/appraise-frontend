c4b1044e16f5d638d4ae6465a50d8af00476de6b
carlosdaniel99 <cdmnkg@gmail.com>
2019-05-13 11:01:13 -0300

    Open and close modal

--- v2/public/src/percival/Certainty.js
+++ v2/public/src/percival/Certainty.js
@@ -1,5 +1,13 @@
 'use  strict'
 
 function showModal(){
-	console.log('SHOW MODAL');
+	var notModalContent = document.getElementById('certainty');
+	notModalContent.classList.remove('force-display-none');
 }
+
+window.addEventListener("click", function (){
+	var notModalContent = document.getElementById('certainty');
+	if (event.target == notModalContent) {
+    document.getElementById('certainty').classList.add('force-display-none');
+  }
+});
--- v2/public/src/percival/Launcher.js
+++ v2/public/src/percival/Launcher.js
@@ -7,7 +7,6 @@ function initLauncher() {
 
   const clientId = '902472309288-h34e3l0vo9e3bkr4dc29f4bco99q0t9s.apps.googleusercontent.com' //Appraise
   // const clientId = '696992508397-9pna2ebcfkt3olr2hukd9q95v21t1iud.apps.googleusercontent.com' //Percival
-
   initAuth(clientId, onUserChanged);
 }
 
