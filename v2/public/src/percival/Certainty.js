e3d2ef8076d801043285d97e912b8444ea3ee2a6
carlosdaniel99 <cdmnkg@gmail.com>
2019-05-13 12:01:09 -0300

    Taking info from cards

--- v2/public/src/percival/Certainty.js
+++ v2/public/src/percival/Certainty.js
@@ -1,8 +1,18 @@
 'use  strict';
 
-function showModal(){
+function showModal(clickedId){
 	var notModalContent = document.getElementById('certainty');
 	notModalContent.classList.remove('force-display-none');
+	console.log(clickedId);
+	// event.dataTransfer.setData('card-id', event.target.id);
+	// var cardId = event.dataTransfer.getData('card-id');
+  var card = document.getElementById(clickedId);
+  var appraiseId = clickedId.substring(5);
+  var namePerson = document.getElementById('name-person');
+  var cardName= namePerson.textContent;
+  var appraiseCertainty = card.getElementsByClassName('active').length * 0.2;
+  //var appraiseLevel = event.target.id.substring(11);
+  console.log(cardName,appraiseId,appraiseCertainty);
 }
 
 window.addEventListener("click", function (){
